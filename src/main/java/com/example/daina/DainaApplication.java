68c232c344e9785dc7067cda8c22d3f2f6a92386
daina <1026395057@qq.com>
2019-03-11 16:46:10 +0800

    新建java项目

---
+++ src/main/java/com/example/daina/DainaApplication.java
@@ -0,0 +1,13 @@
+package com.example.daina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daina/DainaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.daina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DainaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
